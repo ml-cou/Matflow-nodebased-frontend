a839e2f85e6cc1fc8f2487d5e5d9d7981ffa1466
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-08-31 05:30:10 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "p1": "#0A4D68",
-        "p2": "#088395",
-        "p3": "#05BFDB",
-        "p4": "#00FFCA",
-      }
+        p1: "#0A4D68",
+        p2: "#088395",
+        p3: "#05BFDB",
+        p4: "#00FFCA",
+        text: "#0f1419",
+        bg: "#ffffff",
+        "primary-btn": "#00ba7c",
+        "secondary-btn": "#f2f2f2",
+        accent: "#7fdcbd",
+      },
+      fontFamily: {
+        titillium: ["Titillium Web", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
