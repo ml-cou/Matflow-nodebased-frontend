d2746b3e44a32035e1e7cdf5726d91a297ca604d
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-09-08 20:39:01 -0700

    Add files via upload

--- src/util/NodeFunctions.js
+++ src/util/NodeFunctions.js
@@ -1,4 +1,6 @@
 import { toast } from "react-toastify";
+import * as stats from "simple-statistics";
+import * as Stat from "statistics.js";
 
 const EDA_LINK = {
   "Bar Plot": "eda_barplot",
@@ -243,7 +245,41 @@ export const handleMergeDataset = async (rflow, params) => {
     data = JSON.parse(data);
 
     const tempNodes = rflow.getNodes().map((val) => {
-      if (val.id === params.target) return { ...val, data: { table: data } };
+      if (val.id === params.target)
+        return { ...val, data: { table: data, file_name: merge.dataset_name } };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleAppendDataset = async (rflow, params) => {
+  try {
+    const { append } = rflow.getNode(params.source).data;
+
+    if (!append) throw new Error("Check Append Dataset Node.");
+
+    const res = await fetch("http://127.0.0.1:8000/api/append/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(append),
+    });
+
+    let data = await res.json();
+    data = JSON.parse(data);
+
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: append.dataset_name },
+        };
       return val;
     });
     rflow.setNodes(tempNodes);
@@ -257,7 +293,7 @@ export const handleMergeDataset = async (rflow, params) => {
 export const handleAddModify = async (rflow, params) => {
   try {
     let { addModify } = rflow.getNode(params.source).data;
-    console.log(addModify);
+    // console.log(addModify);
     if (!addModify) throw new Error("Check Add/Modify Node.");
     if (addModify.option === "Add") {
       addModify.select_column = addModify.column_name;
@@ -271,12 +307,528 @@ export const handleAddModify = async (rflow, params) => {
     });
 
     let data = await res.json();
-    console.log(data)
+    // console.log(data);
     const tempNodes = rflow.getNodes().map((val) => {
       if (val.id === params.target)
         return {
           ...val,
           data: { table: data, file_name: addModify.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleChangeDtype = async (rflow, params) => {
+  try {
+    let { changeDtype } = rflow.getNode(params.source).data;
+    // console.log(changeDtype);
+    if (!changeDtype) throw new Error("Check Change Dtype Node.");
+
+    const res = await fetch("http://127.0.0.1:8000/api/change_dtype/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(changeDtype),
+    });
+
+    let data = await res.json();
+    // console.log(data);
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: changeDtype.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleAlterFieldName = async (rflow, params) => {
+  try {
+    let { alterFieldName } = rflow.getNode(params.source).data;
+
+    if (!alterFieldName) throw new Error("Check Alter Field Name Node.");
+
+    const res = await fetch("http://127.0.0.1:8000/api/alter_field_name/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(alterFieldName),
+    });
+
+    let data = await res.json();
+    // console.log(data);
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: alterFieldName.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleDropRowColumn = async (rflow, params) => {
+  try {
+    let { dropColumnRow } = rflow.getNode(params.source).data;
+
+    if (!dropColumnRow) throw new Error("Check Drop Row/Column Node.");
+    let url = "http://127.0.0.1:8000/api/drop_column/";
+    if (dropColumnRow.dropOption === "Row")
+      url = "http://127.0.0.1:8000/api/drop_rows/";
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        default_value: dropColumnRow.default_value,
+        select_columns: dropColumnRow.select_columns,
+        file: dropColumnRow.file,
+      }),
+    });
+
+    let data = await res.json();
+    // console.log(data)
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: dropColumnRow.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleScaling = async (rflow, params) => {
+  try {
+    let { scaling } = rflow.getNode(params.source).data;
+
+    if (!scaling) throw new Error("Check Scaling Node.");
+    let url = "http://127.0.0.1:8000/api/scaling/";
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(scaling),
+    });
+
+    let data = await res.json();
+    console.log(data);
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: scaling.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleEncoding = async (rflow, params) => {
+  try {
+    let { encoding } = rflow.getNode(params.source).data;
+
+    if (!encoding) throw new Error("Check Encoding Node.");
+    let url = "http://127.0.0.1:8000/api/encoding/";
+    // console.log(encoding)
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(encoding),
+    });
+
+    let data = await res.json();
+    console.log(data);
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: data, file_name: encoding.dataset_name },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleCluster = async (rflow, params, outputType) => {
+  try {
+    let { cluster } = rflow.getNode(params.source).data;
+
+    if (!cluster) throw new Error("Check Cluster Node.");
+    let url = "http://127.0.0.1:8000/api/cluster/";
+    // console.log(encoding)
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cluster),
+    });
+
+    let data = await res.json();
+    console.log(data);
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: {
+            [outputType === "table" ? "table" : "graph"]:
+              outputType === "table" ? data.table : JSON.parse(data.graph),
+          },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleDatasetInformation = (rflow, params) => {
+  let { table: rowData } = rflow.getNode(params.source).data;
+
+  const columns = Object.keys(rowData[0] || {});
+  const columnInfo = [];
+
+  columns.forEach((column) => {
+    const uniqueValues = new Set();
+    let nonNullCount = 0;
+
+    if (column !== "id") {
+      rowData.forEach((row) => {
+        const value = row[column];
+        if (value !== undefined && value !== null) {
+          uniqueValues.add(value);
+          nonNullCount++;
+        }
+      });
+
+      const nullCount = rowData.length - nonNullCount;
+      const nullPercentage = (nullCount / rowData.length) * 100;
+      const dtype = typeof rowData[0][column];
+
+      columnInfo.push({
+        column,
+        uniqueValues: uniqueValues.size,
+        nonNullCount,
+        nullPercentage,
+        dtype,
+      });
+    }
+  });
+
+  const tempNodes = rflow.getNodes().map((val) => {
+    if (val.id === params.target)
+      return {
+        ...val,
+        data: {
+          table: columnInfo,
+        },
+      };
+    return val;
+  });
+  rflow.setNodes(tempNodes);
+  return true;
+};
+
+export const handleDatasetStatistics = (rflow, params) => {
+  let { table: rowData } = rflow.getNode(params.source).data;
+
+  let columns = Object.keys(rowData[0] || {});
+  const columnStatsData = [];
+  columns = columns.filter((item) => {
+    const dtype = typeof rowData[0][item];
+    return dtype === "number";
+  });
+
+  columns.forEach((column) => {
+    if (column !== "id") {
+      let values = rowData
+        .map((row) => parseFloat(row[column]))
+        .filter((value) => !isNaN(value));
+      const count = values.length;
+      if (count > 0) {
+        const min = stats.min(values).toFixed(3);
+        const max = stats.max(values).toFixed(3);
+        const std = stats.standardDeviation(values).toFixed(3);
+
+        const mean = stats.mean(values).toFixed(3);
+        const percentile25 = stats.quantile(values, 0.25).toFixed(3);
+        const median = stats.quantile(values, 0.5).toFixed(3);
+        const percentile75 = stats.quantile(values, 0.75).toFixed(3);
+
+        columnStatsData.push({
+          column,
+          count,
+          min,
+          max,
+          std,
+          mean,
+          "25%": percentile25,
+          "50%": median,
+          "75%": percentile75,
+        });
+      }
+    }
+  });
+
+  const tempNodes = rflow.getNodes().map((val) => {
+    if (val.id === params.target)
+      return {
+        ...val,
+        data: {
+          table: columnStatsData,
+        },
+      };
+    return val;
+  });
+  rflow.setNodes(tempNodes);
+  return true;
+};
+
+export const handleDatasetCorrelation = async (rflow, params, outputType) => {
+  try {
+    const { table: rowData, correlation } = rflow.getNode(params.source).data;
+
+    if (!correlation) throw new Error("Check Corelation Node");
+
+    const calculateCorrelations = (data) => {
+      let columnNames = Object.keys(data[0]);
+      columnNames = columnNames.filter(
+        (val) => typeof data[0][val] === "number" && val !== "id"
+      );
+      const correlations = {};
+
+      for (let i = 0; i < columnNames.length; i++) {
+        const column1 = columnNames[i];
+        correlations[column1] = {};
+
+        for (let j = 0; j < columnNames.length; j++) {
+          const column2 = columnNames[j];
+          const column1Data = [];
+          const column2Data = [];
+          const tempData = [];
+
+          for (let k = 0; k < data.length; k++) {
+            const val1 = parseFloat(data[k][column1]);
+            const val2 = parseFloat(data[k][column2]);
+
+            if (!isNaN(val1) && !isNaN(val2)) {
+              column1Data.push(val1);
+              column2Data.push(val2);
+
+              tempData.push({
+                [column1]: val1,
+                [column2]: val2,
+              });
+            }
+          }
+
+          const bodyVars = {
+            [column1]: "metric",
+            [column2]: "metric",
+          };
+
+          const temp = new Stat(tempData, bodyVars);
+          // Calculate the correlation coefficient using simple-statistics correlation function
+          const l = Object.keys(bodyVars);
+
+          if (correlation.method === "spearman") {
+            const cc = temp.spearmansRho(l[0], l[l.length === 1 ? 0 : 1], true);
+
+            // // Store the correlation coefficient in the correlations object
+            correlations[column1][column2] = cc.rho.toFixed(3);
+          } else if (correlation.method === "pearson") {
+            const cc = temp.correlationCoefficient(
+              l[0],
+              l[l.length === 1 ? 0 : 1]
+            );
+
+            // // Store the correlation coefficient in the correlations object
+            correlations[column1][column2] =
+              cc.correlationCoefficient.toFixed(3);
+          }
+        }
+      }
+
+      return correlations;
+    };
+
+    let cor = calculateCorrelations(rowData);
+
+    if (correlation.method === "kendall") {
+      const resp = await fetch(
+        "http://127.0.0.1:8000/api/display_correlation/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            file: rowData,
+          }),
+        }
+      );
+      let { data } = await resp.json();
+
+      cor = JSON.parse(data);
+      let columnName = Object.keys(cor[0]);
+      columnName = columnName.filter((val) => val !== "id");
+      let newData = [];
+      for (let i = 0; i < columnName.length; i++) {
+        const { id, ...rest } = cor[i];
+        newData.push({ ...rest, column_name: columnName[i] });
+      }
+      console.log({ newData });
+      cor = newData;
+    } else {
+      let ind = 0;
+      const columnName = Object.keys(cor);
+      cor = Object.values(cor);
+      cor = cor.map((val) => {
+        return { ...val, column_name: columnName[ind++] };
+      });
+    }
+
+    const columnNames = Object.keys(rowData[0]);
+    let colWithInd = {};
+    for (let i = 0; i < columnNames.length; i++) {
+      colWithInd = { ...colWithInd, [columnNames[i]]: i };
+    }
+
+    const columnSelected = new Set(correlation.show_column);
+
+    let tempData = JSON.parse(JSON.stringify(cor));
+    for (let i = 0; i < columnNames.length; i++) {
+      if (columnSelected.has(columnNames[i])) continue;
+      const colInd = colWithInd[columnNames[i]];
+      for (let j = 0; j < tempData.length; j++) {
+        if (colInd === j) tempData[j] = {};
+        delete tempData[j][columnNames[i]];
+      }
+    }
+    console.log(tempData);
+    tempData = tempData.filter((val) => Object.keys(val).length !== 0);
+
+    cor = tempData;
+
+    let graphData = undefined;
+    if (outputType === "graph") {
+      const res = await fetch(
+        "http://127.0.0.1:8000/api/display_correlation_heatmap/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            file: cor,
+          }),
+        }
+      );
+      const data = await res.json();
+      graphData = JSON.parse(data);
+    }
+
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: {
+            [outputType === "table" ? "table" : "graph"]:
+              outputType === "table" ? cor : graphData,
+          },
+        };
+      return val;
+    });
+    rflow.setNodes(tempNodes);
+
+    return true;
+  } catch (error) {
+    raiseErrorToast(rflow, params, error.message);
+    return false;
+  }
+};
+
+export const handleDatasetGroup = async (rflow, params) => {
+  try {
+    let { group } = rflow.getNode(params.source).data;
+
+    if (!group) throw new Error("Check Group Node.");
+    let url = "http://127.0.0.1:8000/api/display_group/";
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        file: group.file,
+        group_var: group.selectedColumn,
+        agg_func: group.agg_func,
+      }),
+    });
+
+    let { data } = await res.json();
+    console.log(JSON.parse(data));
+    const tempNodes = rflow.getNodes().map((val) => {
+      if (val.id === params.target)
+        return {
+          ...val,
+          data: { table: JSON.parse(data) },
         };
       return val;
     });
